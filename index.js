56d7bfeb76a00960998e3ec8205af704df1d16fd
Samisahariar <samisahariar69@gmail.com>
2024-06-21 08:26:17 -0700

    this is the third commit

--- index.js
+++ index.js
@@ -5,10 +5,16 @@ const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require("dotenv").config()
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+
 
 //middlewares
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173"],
+  credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser());
 
 const username = process.env.DB_MONGO_USER;
 const password = process.env.DB_MONGO_KEY;
@@ -24,34 +30,90 @@ const client = new MongoClient(uri, {
   }
 });
 
+
+
+const cookieOptions = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+};
+
+
+const verifyToken = (req, res, next) => {
+  const token = req.cookies?.token;
+  if (!token) {
+    return res.status(401).send({ message: 'unauthorized' })
+  };
+  jwt.verify(token, process.env.DB_ACESS_TOKEN , (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: 'unauthorized access' })
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
+
+const verifyAdmin = async (req, res, next) => {
+  const email = req.decoded.email;
+  const query = { email: email };
+  const user = await userCollection.findOne(query);
+  const isAdmin = user?.role === 'admin';
+  if (!isAdmin) {
+    return res.status(403).send({ message: 'forbidden access' });
+  }
+  next();
+}
+
+
+
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     //await client.connect();
     // Send a ping to confirm a successful connection
 
-    app.post("jwt", async (req, res) => {
+    //ALl of the collection is here below the table : 
+    const userCollection = client.db("assignment12").collection("users")
+
+
+
+
+
+    //token related
+    app.post("/jwt", async (req, res) => {
       const user = req.body;
       const token = jwt.sign(user, process.env.DB_ACESS_TOKEN, { expiresIn: '1h' });
-      res.send({ token });
+      res
+        .cookie('token', token, cookieOptions)
+        .send({ success: true })
     })
 
-    //middlewares
-    const verifyToken = (req, res, next) => {
-      // console.log('inside verify token', req.headers.authorization);
-      if (!req.headers.authorization) {
-        return res.status(401).send({ message: 'unauthorized access' });
-      }
-      const token = req.headers.authorization.split(' ')[1];
-      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-        if (err) {
-          return res.status(401).send({ message: 'unauthorized access' })
-        }
-        req.decoded = decoded;
-        next();
-      })
-    }
 
+    app.post('/logout', async (req, res) => {
+      const loggedUser = req.body;
+      res.clearCookie('token', { ...cookieOptions, maxAge: 0 }).send({ message: "cookie clear successful" })
+    })
+
+
+    //users collection is here and all of the user is processed here
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email }
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: 'user already exists', insertedId: null })
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+
+    app.get("/users", verifyToken,  async (req, res) => {
+      const users = await userCollection.find().toArray()
+      res.send(users)
+    })
 
 
 
